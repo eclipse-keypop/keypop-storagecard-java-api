752b772a84ebcbcac7f81181531d2d694fb35b18
Vincent Ducrot <vincent.ducrot@cis-france.net>
2026-03-18 14:57:17 +0100

    feat: add method prepareReadSystemBlock to StorageCardSelectionExtension (#10)

--- src/main/java/org/eclipse/keypop/storagecard/card/StorageCardSelectionExtension.java
+++ src/main/java/org/eclipse/keypop/storagecard/card/StorageCardSelectionExtension.java
@@ -57,6 +57,22 @@ public interface StorageCardSelectionExtension extends CardSelectionExtension {
   StorageCardSelectionExtension prepareReadBlocks(int fromBlockAddress, int toBlockAddress);
 
   /**
+   * Prepares the reading of the system block from an ST25/SRT512 storage card.
+   *
+   * <p>This method is specific to ST25 and SRT512 card types which provide access to a system block
+   * at address 255 containing card-specific metadata and configuration data.
+   *
+   * <p>Once this command is processed, the result is available in {@link StorageCard} via {@link
+   * StorageCard#getSystemBlock()} method.
+   *
+   * @return The current instance.
+   * @throws UnsupportedOperationException If the current card type is not ST25/SRT512.
+   * @see StorageCard#getSystemBlock()
+   * @since 1.2.0
+   */
+  StorageCardSelectionExtension prepareSt25ReadSystemBlock();
+
+  /**
    * Prepares a Mifare Classic authentication command using a provided key.
    *
    * <p>This method is specific to Mifare Classic cards and must be called before reading from or
